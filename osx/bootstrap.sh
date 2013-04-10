075e8500ecd1d6a1cee06f472115c821a44972ba
Edward Mann <the@eddmann.com>
2013-04-10 08:47:15 +0100

    update osx bootstrap subl location.

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,3 +1,3 @@
 # Add Sublime Text 2 console bin symlink.
 echo "Creating symlink to Sublime Text 2 console binary."
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/test/bin/subl
+ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
